00912d2e987a450ab3c788ee2c91d22d1a45a00f
Mauricio <mau.uehara@gmail.com>
2013-10-18 16:31:01 -0300

    Add email and password-digest columns to user db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131005212829) do
+ActiveRecord::Schema.define(version: 20131018192718) do
 
   create_table "ratings", force: true do |t|
     t.integer  "user_id"
@@ -28,10 +28,12 @@ ActiveRecord::Schema.define(version: 20131005212829) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "name"
     t.string   "yelp_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
   end
 
 end
