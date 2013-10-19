c11dcbe9978199fee318ff51bf117eaa02e4b7ba
Mauricio <mau.uehara@gmail.com>
2013-10-19 09:15:59 -0300

    Redirect user to root when signed in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to user
+			redirect_to root_path
 		else
 			flash.now[:alert] = 'Invalid email/password combination'
 			render 'new'
