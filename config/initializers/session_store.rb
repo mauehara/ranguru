813cdb930138b55660558a0eaecd7c6b4f18812f
Gabriela Botelho <gabriela.cbotelho@gmail.com>
2013-11-20 01:24:33 -0200

    Fixing Cookie Overflow

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-Ranguru::Application.config.session_store :cookie_store, key: '_ranguru_session'
+Ranguru::Application.config.session_store :active_record_store
+
+module ActionDispatch
+	module Routing
+		class RouteSet
+			def inspect
+				"nothing to see here"
+			end
+		end
+	end
+end
